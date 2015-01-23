86f3f626832cb410f86eacc82f566dad819c108f
n3rdz <n3rdz@users.noreply.github.com>
2015-01-23 20:37:01 +0100

    Update addon.py

--- addon.py
+++ addon.py
@@ -7,7 +7,7 @@ addon_handle = int(sys.argv[1])
 xbmcplugin.setContent(addon_handle, 'movies')
 
 url = 'http://localhost/some_video.mkv'
-li = xbmcgui.ListItem('My First Video!', iconImage='DefaultVideo.png')
+li = xbmcgui.ListItem(label='Livestream', iconImage='icon.png', thumbnailImage='thumbnail.png')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 
 xbmcplugin.endOfDirectory(addon_handle)
