e723755221b3fbd75b14bb89cdc08ed1d5e068cd
n3rdz <n3rdz@users.noreply.github.com>
2015-01-23 20:25:29 +0100

    Update addon.py

--- addon.py
+++ addon.py
@@ -1,20 +1,13 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-import urllib,urllib2,re,xbmcplugin,xbmcgui,sys,xbmcaddon,base64
-pluginhandle = int(sys.argv[1])
-xbox = xbmc.getCondVisibility("System.Platform.xbox")
-settings = xbmcaddon.Addon(id='plugin.DASDING')
-translation = settings.getLocalizedString
-forceViewMode=settings.getSetting("forceViewMode")
-if forceViewMode=="true":
-forceViewMode=True
-else:
-forceViewMode=False
-viewMode=str(settings.getSetting("viewMode"))
-def index():
-addDir("Visual Radio","http://mp3-live.dasding.de/dasding_m.m3u',"")
-elif mode == 'playAudio':
-playAudio(url)
-else:
- index()
- 
+import sys
+import xbmcgui
+import xbmcplugin
+
+addon_handle = int(sys.argv[1])
+
+xbmcplugin.setContent(addon_handle, 'movies')
+
+url = 'http://localhost/some_video.mkv'
+li = xbmcgui.ListItem('My First Video!', iconImage='DefaultVideo.png')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+xbmcplugin.endOfDirectory(addon_handle)
