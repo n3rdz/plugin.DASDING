11ff660d646364ebc7df3aed832c76ab634e6f37
n3rdz <n3rdz@users.noreply.github.com>
2015-01-25 19:57:50 +0100

    Update addon.py

--- addon.py
+++ addon.py
@@ -7,7 +7,7 @@ addon_handle = int(sys.argv[1])
 xbmcplugin.setContent(addon_handle, 'audio')
 
 url = 'http://mp3-live.dasding.de/dasding_m.m3u'
-li = xbmcgui.ListItem(label='Livestream Radio', iconImage='icon.png', thumbnailImage=iconimage)
+li = xbmcgui.ListItem(label='Livestream Radio', iconImage='icon.png')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 
 xbmcplugin.endOfDirectory(addon_handle)
