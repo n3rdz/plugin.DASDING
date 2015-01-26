7928e62d377409e3739fa098d66b7ad98be21c6e
n3rdz <n3rdz@users.noreply.github.com>
2015-01-26 17:49:53 +0100

    Update addon.py
    set icon
    set tumbnail

--- addon.py
+++ addon.py
@@ -5,6 +5,8 @@ addon_handle = int(sys.argv[1])
 xbmcplugin.setContent(addon_handle, 'movies')
 url = 'http://swrdasding-lh.akamaihd.net/i/dasdingvisual_live@6416/master.m3u8'
 li = xbmcgui.ListItem(label='HLS Stream Visual Radio')
+li.setIconImage('icon.png')
+li.setThumbnailImage('icon.png')
 li.setProperty('fanart_image', 'visualradio.jpg')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 xbmcplugin.endOfDirectory(addon_handle)
