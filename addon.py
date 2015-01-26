fa86fedde0d412ea42af5d50282893f4db3c9f7b
n3rdz <n3rdz@users.noreply.github.com>
2015-01-26 17:11:29 +0100

    Update addon.py
    images wieder in einer zeile, da noch immer korrupt

--- addon.py
+++ addon.py
@@ -7,9 +7,7 @@ addon_handle = int(sys.argv[1])
 xbmcplugin.setContent(addon_handle, 'video')
 
 url = 'http://swrdasding-lh.akamaihd.net/i/dasdingvisual_live@6416/master.m3u8'
-li = xbmcgui.ListItem(label='HLS Stream Visual Radio')
-li.setIconImage('icon.png')
-li.setThumbnailImage('icon.png')
+li = xbmcgui.ListItem(label='HLS Stream Visual Radio', iconImage='icon.png', thumbnailImage='icon.png')
 li.setProperty('fanart_image', 'visualradio.png')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 
