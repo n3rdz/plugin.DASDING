255baf5ec2d5519c784acff2539420c1f0ee41ec
n3rdz <n3rdz@users.noreply.github.com>
2015-01-26 17:01:52 +0100

    Update addon.py
    fanart und icons werden in eigenen zeilen eingefügt

--- addon.py
+++ addon.py
@@ -7,10 +7,15 @@ addon_handle = int(sys.argv[1])
 xbmcplugin.setContent(addon_handle, 'audio')
 
 url = 'http://swrdasding-lh.akamaihd.net/i/dasdingvisual_live@6416/master.m3u8'
-li = xbmcgui.ListItem(label='HLS Stream Visual Radio', iconImage='icon.png', thumbnailImage='visualradio.png')
+li = xbmcgui.ListItem(label='HLS Stream Visual Radio')
+li.setIconImage('icon.png')
+li.setThumbnailImage('icon.png')
+li.setProperty('fanart_image', 'visualradio.png')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 url = 'http://swrdasding-lh.akamaihd.net/z/dasdingvisual_live@6416/manifest.f4m'
-li = xbmcgui.ListItem(label='HDS Stream Visual Radio', iconImage='icon.png', thumbnailImage='visualradio.png')
+li = xbmcgui.ListItem(label='HDS Stream Visual Radio')
+li.setIconImage('icon.png')
+li.setThumbnailImage('icon.png')
 li.setProperty('fanart_image', 'visualradio.png')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 
