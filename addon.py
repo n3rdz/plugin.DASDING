2b5dd08f69e3c7aedb3dbad7a7fcb6d01bfe1fe3
n3rdz <n3rdz@users.noreply.github.com>
2015-01-26 17:47:02 +0100

    Update addon.py
    set fanart in line 8

--- addon.py
+++ addon.py
@@ -4,6 +4,7 @@ import xbmcplugin
 addon_handle = int(sys.argv[1])
 xbmcplugin.setContent(addon_handle, 'movies')
 url = 'http://swrdasding-lh.akamaihd.net/i/dasdingvisual_live@6416/master.m3u8'
-li = xbmcgui.ListItem(label='HLS Stream Visual Radio', iconImage='icon.png', thumbnailImage='visualradio.png')
+li = xbmcgui.ListItem(label='HLS Stream Visual Radio')
+li.setProperty('fanart_image', 'visualradio.jpg')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 xbmcplugin.endOfDirectory(addon_handle)
