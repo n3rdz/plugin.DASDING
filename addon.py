7af3e8fefdb4c4e0982531afd1ce47bfb6297002
n3rdz <n3rdz@users.noreply.github.com>
2015-01-26 18:44:44 +0100

    Update addon.py

--- addon.py
+++ addon.py
@@ -6,7 +6,7 @@ xbmcplugin.setContent(addon_handle, 'audio')
 
 url = 'http://swrdasding-lh.akamaihd.net/i/dasdingvisual_live@6416/master.m3u8'
 li = xbmcgui.ListItem(label='DASDING Visual Radio HLS Stream m3u8')
-li.setIconImage('icon.png')
+li.setIconImage('DefaultVideo.png')
 li.setThumbnailImage('icon.png')
 li.setProperty('fanart_image', 'visualradio.png')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
