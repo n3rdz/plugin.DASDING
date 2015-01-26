950a60d228c29efce2f2d1b591afc6bace2dd8a7
n3rdz <n3rdz@users.noreply.github.com>
2015-01-26 17:43:02 +0100

    Update addon.py
    set handle to movies

--- addon.py
+++ addon.py
@@ -2,11 +2,8 @@ import sys
 import xbmcgui
 import xbmcplugin
 addon_handle = int(sys.argv[1])
-xbmcplugin.setContent(addon_handle, 'audio')
+xbmcplugin.setContent(addon_handle, 'movies')
 url = 'http://swrdasding-lh.akamaihd.net/i/dasdingvisual_live@6416/master.m3u8'
 li = xbmcgui.ListItem(label='HLS Stream Visual Radio', iconImage='icon.png', thumbnailImage='visualradio.png')
-xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
-url = 'http://swrdasding-lh.akamaihd.net/z/dasdingvisual_live@6416/manifest.f4m'
-li = xbmcgui.ListItem(label='HDS Stream Visual Radio', iconImage='icon.png', thumbnailImage='visualradio.png')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 xbmcplugin.endOfDirectory(addon_handle)
